519d8c3c9e7549d5a9f90768306a38e0cd789a27
IMROCK <ALEXXXL1MM@mail.ru>
2025-05-21 15:32:39 +0300

    ╨░╨▓╤В╨╛╨╛╨▒╨╜╨╛╨▓╨╗╨╡╨╜╨╕╨╡

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showScreen(name) {
   const screens = document.querySelectorAll('.screen');
-  screens.forEach(screen => screen.style.display = 'none');
+  screens.forEach(screen => (screen.style.display = 'none'));
   const target = document.getElementById(name);
   if (target) {
     target.style.display = 'block';
