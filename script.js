61170f701844217efd1e04e52434f4ad7952c9eb
IMROCK <ALEXXXL1MM@mail.ru>
2025-05-23 11:46:48 +0300

    AUTO

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("inventory").onclick = () => openModal("inventory");
   document.getElementById("market").onclick = () => openModal("market");
   document.getElementById("mining").onclick = () => openModal("mining");
-
-  // HOME — просто закрывает модалку
   document.getElementById("home").onclick = () => {
     modalContainer.innerHTML = "";
   };
